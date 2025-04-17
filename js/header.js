9085befef8b90fd4a52fb3b5669cb69c3c7eb9ac
meri-sargsyan1 <meri.sargsyan.08@mail.ru>
2025-04-17 08:41:27 +0400

    emmy

---
+++ js/header.js
@@ -0,0 +1,43 @@
+const langDisplay = document.getElementById('selected-language');
+const savedLanguage = localStorage.getItem('selectedLanguage');
+
+
+if (savedLanguage) {
+    langDisplay.innerText = savedLanguage;
+}
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        const selectedLang = this.getAttribute('data-lang');
+        langDisplay.innerText = selectedLang;
+        localStorage.setItem('selectedLanguage', selectedLang);
+    });
+});
+
+
+
+
+
+const currencyDisplay = document.getElementById('selected-currency');
+const priceCurrency = document.getElementById('price-currency');
+const savedCurrency = localStorage.getItem('selectedCurrency');
+
+if (savedCurrency) {
+    currencyDisplay.innerText = savedCurrency;
+}
+
+
+document.querySelectorAll('.dropdown-menu a[data-currency]')
+.forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+        const selectedCurrency = this.getAttribute('data-currency');
+
+
+        currencyDisplay.innerText = selectedCurrency;
+        priceCurrency.innerText = selectedCurrency;
+
+        localStorage.setItem('selectedCurrency', selectedCurrency);
+    });
+});
